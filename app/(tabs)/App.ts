5e3577daa5d2da4a031d336baa8deb2392af2b71
geoduck79 <johnhubertchen@gmail.com>
2026-02-25 10:41:54 +0700

    app.ts , data.json , index.tsx

---
+++ app/(tabs)/App.ts
@@ -0,0 +1,38 @@
+
+import { StyleSheet} from "react-native";
+
+
+const styles = StyleSheet.create({
+  container :{
+    backgroundColor : "#ffff",
+    alignItems : "center",
+    justifyContent :"center",
+    padding : 5 ,
+    display : "flex" ,
+
+  },
+  card : {
+    borderWidth : 1 ,
+    borderColor : "black",
+    borderRadius : 8 ,
+    display : "flex",
+    flexDirection :"row",
+    alignItems : "center",
+    padding : 8 ,
+    width : 325 , 
+    gap : 8 ,
+
+  },
+  avatar : {
+    width : 75,
+    height: 75 , 
+    borderRadius : 999 , 
+
+  },
+  boldText: {
+    fontWeight : "bold",
+
+  },
+});
+
+export default styles ; 
